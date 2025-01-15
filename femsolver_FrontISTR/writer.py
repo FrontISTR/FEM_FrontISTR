ede9f2d990b303f7f4b666b62d06c357837a2230
Kanato Kono <kanato@chikasuiro.dev>
2025-01-15 12:19:35 +0900

    Preserve writer for FreeCAD 0.xx

--- femsolver_FrontISTR/writer.py
+++ femsolver_FrontISTR/writer.py
@@ -723,18 +723,31 @@ class FemInputWriterfistr(writerbase.FemInputWriter):
             selwei_obj = femobj["Object"]
             f.write("## " + selwei_obj.Label + "\n")
             f.write("!DLOAD,GRPID=1\n")
-            f.write(
-                # elset, GRAV, magnitude, direction x, dir y ,dir z
-                "{},GRAV,{:.13G},{:.13G},{:.13G},{:.13G}\n".format(
-                    self.fistr_eall,
-                    selwei_obj.GravityAcceleration.getValueAs(
-                        "mm/s^2"
-                    ).Value,  # actual magnitude of gravity vector
-                    selwei_obj.GravityDirection.x,  # coordinate x of normalized gravity vector
-                    selwei_obj.GravityDirection.y,  # y
-                    selwei_obj.GravityDirection.z   # z
+            if int(self.fc_ver[0]) >= 1:
+                f.write(
+                    # elset, GRAV, magnitude, direction x, dir y ,dir z
+                    "{},GRAV,{:.13G},{:.13G},{:.13G},{:.13G}\n".format(
+                        self.fistr_eall,
+                        selwei_obj.GravityAcceleration.getValueAs(
+                            "mm/s^2"
+                        ).Value,  # actual magnitude of gravity vector
+                        selwei_obj.GravityDirection.x,  # coordinate x of normalized gravity vector
+                        selwei_obj.GravityDirection.y,  # y
+                        selwei_obj.GravityDirection.z   # z
+                    )
                 )
-            )
+            else:
+                f.write(
+                    # elset, GRAV, magnitude, direction x, dir y ,dir z
+                    "{},GRAV,{},{},{},{}\n"
+                    .format(
+                        " "+self.fistr_eall,
+                        self.gravity,  # actual magnitude of gravity vector
+                        selwei_obj.Gravity_x,  # coordinate x of normalized gravity vector
+                        selwei_obj.Gravity_y,  # y
+                        selwei_obj.Gravity_z  # z
+                    )
+                )
             f.write("\n")
         # grav (erdbeschleunigung) is equal for all elements
         # should be only one constraint
@@ -771,15 +784,26 @@ class FemInputWriterfistr(writerbase.FemInputWriter):
                 f.write(f"## {ref_shape[0]}\n")
                 for n in sorted(ref_shape[1]):
                     node_load = ref_shape[1][n]
-                    if abs(direction_vec.x) > dir_zero_tol:
-                        v1 = f"{(direction_vec.x * node_load).Value:.13G}"
-                        f.write(f"{n},1,{v1}\n")
-                    if abs(direction_vec.y) > dir_zero_tol:
-                        v2 = f"{(direction_vec.y * node_load).Value:.13G}"
-                        f.write(f"{n},2,{v2}\n")
-                    if abs(direction_vec.z) > dir_zero_tol:
-                        v3 = f"{(direction_vec.z * node_load).Value:.13G}"
-                        f.write(f"{n},3,{v3}\n")
+                    if int(self.fc_ver[0]) >= 1:
+                        if abs(direction_vec.x) > dir_zero_tol:
+                            v1 = f"{(direction_vec.x * node_load).Value:.13G}"
+                            f.write(f"{n},1,{v1}\n")
+                        if abs(direction_vec.y) > dir_zero_tol:
+                            v2 = f"{(direction_vec.y * node_load).Value:.13G}"
+                            f.write(f"{n},2,{v2}\n")
+                        if abs(direction_vec.z) > dir_zero_tol:
+                            v3 = f"{(direction_vec.z * node_load).Value:.13G}"
+                            f.write(f"{n},3,{v3}\n")
+                    else:
+                        if (direction_vec.x != 0.0):
+                            v1 = "{:.13E}".format(direction_vec.x * node_load)
+                            f.write(str(n) + ",1," + v1 + "\n")
+                        if (direction_vec.y != 0.0):
+                            v2 = "{:.13E}".format(direction_vec.y * node_load)
+                            f.write(str(n) + ",2," + v2 + "\n")
+                        if (direction_vec.z != 0.0):
+                            v3 = "{:.13E}".format(direction_vec.z * node_load)
+                            f.write(str(n) + ",3," + v3 + "\n")
                 f.write("\n")
             f.write("\n")
 
@@ -808,9 +832,12 @@ class FemInputWriterfistr(writerbase.FemInputWriter):
             prs_obj = femobj["Object"]
             f.write("## " + prs_obj.Label + "\n")
             rev = -1 if prs_obj.Reversed else 1
-            # the pressure has to be output in MPa
-            pressure_quantity = FreeCAD.Units.Quantity(prs_obj.Pressure.getValueAs("MPa"))
-            press_rev = rev * pressure_quantity
+            if int(self.fc_ver[0]) >= 1:
+                # the pressure has to be output in MPa
+                pressure_quantity = FreeCAD.Units.Quantity(prs_obj.Pressure.getValueAs("MPa"))
+                press_rev = rev * pressure_quantity
+            else:
+                press_rev = rev * prs_obj.Pressure
             f.write("!DLOAD,GRPID=1\n")
             for ref_shape in femobj["PressureFaces"]:
                 # the loop is needed for compatibility reason
